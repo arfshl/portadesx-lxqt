25b2e81a7239a4faa301042416eac8d7333c9b06
Alif <arfshl@protonmail.com>
2025-08-23 11:00:14 +0700

    Update vncsetup.sh

--- scripts/vncsetup.sh
+++ scripts/vncsetup.sh
@@ -27,7 +27,7 @@ export USER=portadesx
 export HOME=/home/portadesx
 vncserver -kill :4
 rm -rf /home/portadesx/.vnc/localhost:4.pid
-rm -rf /tmp/.X1-lock
+rm -rf /tmp/.X4-lock
 rm -rf /tmp/.X11-unix/X4' >> /usr/local/bin/stopvnc
 
 # Create script for restarting VNC server
