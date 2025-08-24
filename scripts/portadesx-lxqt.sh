42a2163e8397dd77081e2a1f750b901f8406e769
Alif <arfshl@protonmail.com>
2025-08-24 10:28:43 +0700

    Update portadesx-lxqt.sh

--- scripts/portadesx-lxqt.sh
+++ scripts/portadesx-lxqt.sh
@@ -1,6 +1,6 @@
 DISTRO_NAME="PortadesX:LXQt"
 TARBALL_URL['aarch64']="https://github.com/portadesx/portadesx-lxqt/releases/download/noble-aarch64/portadesx-lxqt.tar.xz"
-TARBALL_SHA256['aarch64']="76c2e3b2b965b4deb6938425334fba39cf48c1f6be3c723d6895b3aeb7d48639"
+TARBALL_SHA256['aarch64']="a9f685098c0a880463e5161defd56c4bdd93ca9113ced4997585611e3e1fabc1"
 distro_setup() {
         run_proot_cmd ln -s /storage/emulated/0/ /home/portadesx/Desktop/android_files
 }
