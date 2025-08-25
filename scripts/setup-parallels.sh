05c2cdf81b99617d6f8f386d73ae5708b8b72eb1
Alif <arfshl@protonmail.com>
2025-08-25 11:19:42 +0700

    Update setup-parallels.sh

--- scripts/setup-parallels.sh
+++ scripts/setup-parallels.sh
@@ -4,7 +4,7 @@ wget https://raw.githubusercontent.com/portadesx/portadesx-lxqt/refs/heads/main/
 
 # Create startup script
 # for CLI session
-printf 'proot-distro login portadesx --user portadesx' >> /data/data/com.termux/files/usr/bin/portadesx-lxqt-cli
+printf 'proot-distro login portadesx-lxqt --user portadesx' >> /data/data/com.termux/files/usr/bin/portadesx-lxqt-cli
 
 # for X11 session
 cat <<EOF > /data/data/com.termux/files/usr/bin/portadesx-lxqt-gui
